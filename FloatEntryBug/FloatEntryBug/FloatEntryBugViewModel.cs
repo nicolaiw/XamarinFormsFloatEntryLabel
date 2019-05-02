c76f306ba3280b837a38fe2e01d03e42cabb5eea
WIRTZNI <WIRTZNI@PC4461>
2019-05-02 11:27:17 +0200

    upload

---
+++ FloatEntryBug/FloatEntryBug/FloatEntryBugViewModel.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace FloatEntryBug
+{
+    public class FloatEntryBugViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private float _floatProp;
+        public float FloatProp
+        {
+            get
+            {
+                return _floatProp;
+            }
+            set
+            {
+                _floatProp = value;
+                OnPropertyChanged(nameof(FloatProp));
+            }
+        }
+
+        private double _doubleProp;
+        public double DoubleProp
+        {
+            get
+            {
+                return _doubleProp;
+            }
+            set
+            {
+                _doubleProp = value;
+                OnPropertyChanged(nameof(DoubleProp));
+            }
+        }
+
+        private decimal _decimalProp;
+        public decimal DecimalProp
+        {
+            get
+            {
+                return _decimalProp;
+            }
+            set
+            {
+                _decimalProp = value;
+                OnPropertyChanged(nameof(DecimalProp));
+            }
+        }
+
+        private string _appCulture;
+        public string AppCulture
+        {
+            get
+            {
+                return _appCulture;
+            }
+            set
+            {
+                _appCulture = value;
+                OnPropertyChanged(nameof(AppCulture));
+            }
+        }
+
+        private string _appUiCulture;
+        public string AppUiCulture
+        {
+            get
+            {
+                return _appUiCulture;
+            }
+            set
+            {
+                _appUiCulture = value;
+                OnPropertyChanged(nameof(AppUiCulture));
+            }
+        }
+
+
+        public FloatEntryBugViewModel(float floatVal, double doubleVal, decimal decimalVal)
+        {
+            FloatProp = floatVal;
+            DoubleProp = doubleVal;
+            DecimalProp = decimalVal;
+
+            AppCulture = System.Threading.Thread.CurrentThread.CurrentCulture.ToString();
+            AppUiCulture = System.Threading.Thread.CurrentThread.CurrentUICulture.ToString();
+        }
+    }
+}
---
+++ FloatEntryBug/FloatEntryBug/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FloatEntryBug
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new FloatEntryBugViewModel(27.4f, 27.4, 27.4m);
+        }
+    }
+}
